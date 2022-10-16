d4023b389c076c0007e4d0280d9fcab649963896
DIMECK Raphael <raphael.dimeck@etu.umontpellier.fr>
2022-10-16 12:20:27 +0200

    maj dossiers

---
+++ src/Controller/ControllerUtilisateur.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Vote\Controller;
+
+class ControllerUtilisateur
+{
+
+}
---
+++ src/Model/DataObject/Question.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Vote\Model\DataObject;
+
+class Question
+{
+    private string $titre;
+    private array $sections;
+
+}
